366655f7a338241a9d962d3189fd451407043300
XiaoxingChen <cxx@seer-robotics.com>
2017-10-24 23:17:46 +0800

    modify: finish remove test

--- fixed_vector_unit_test/main.cpp
+++ fixed_vector_unit_test/main.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 #include "fixed_vector.h"
 #include <stdint.h>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
-#define VECT_LEN 10
+#define VECT_LEN 50
 #define VECT_TYPE uint16_t
-fixed_vector<VECT_TYPE, VECT_LEN> tv;
+
+void remove_test();
 
 void print_vector(fixed_vector<VECT_TYPE, VECT_LEN>& v)
 {
+    cout << "fixed_vector:\t";
     for (int i = 0; i < v.size(); i++)
     {
         cout << v.at(i) << " ";
@@ -16,17 +20,62 @@ void print_vector(fixed_vector<VECT_TYPE, VECT_LEN>& v)
     cout << endl;
 }
 
+void print_vector(vector<VECT_TYPE>& v)
+{
+    cout << "std_vector:\t";
+	for(int i = 0; i < v.size(); i++)
+	{
+		cout << v.at(i) << " ";
+	}
+	cout << endl;
+
+}
+
 int main()
 {
     /*cin.get();*/
-    cout << "hello" << endl;
+    cout << "start" << endl;
     /*cin.get();*/
-    tv.push_back(10);
-    tv.push_back(10);
-    tv.push_back(10);
-    tv.push_back(10);
-    print_vector(tv);
+    remove_test();
     
     cin.get();
 	return 0;
+}
+
+void remove_test()
+{
+    const uint8_t test_buf_len = 30;
+	VECT_TYPE originArray[test_buf_len];
+    vector<VECT_TYPE> stdv;
+    fixed_vector<VECT_TYPE, VECT_LEN> v;
+
+    for (int i = 0; i < test_buf_len; i++)
+    {
+        originArray[i] = rand() % 10;
+        stdv.push_back(originArray[i]);
+        v.push_back(originArray[i]);
+    }
+
+    VECT_TYPE remove_num = 5;
+    remove(stdv.begin(), stdv.end(), remove_num);
+    v.remove(remove_num);
+
+    bool test_ok = true;
+   
+    do 
+    {
+        if (v.size() != stdv.size())
+        {
+            test_ok = false;
+            cout << "test faild, length mismatch" << endl;
+            break;
+        }
+    
+    } while (0);
+
+    if (!test_ok)
+    {
+        print_vector(v);
+        print_vector(stdv);
+    }
 }
