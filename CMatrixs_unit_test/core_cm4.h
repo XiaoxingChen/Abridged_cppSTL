14e8a7e035ac66b2aa3d5a2bb6ddf7979b7f7786
XiaoxingChen <cxx@seer-robotics.com>
2017-11-05 23:15:11 +0800

    Modify: inverse test and add test for CMatrixs

---
+++ CMatrixs_unit_test/core_cm4.h
@@ -0,0 +1,76 @@
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+#ifndef __CORE_CM4_H_GENERIC
+#define __CORE_CM4_H_GENERIC
+
+#include <stdint.h>
+#define __INLINE inline
+#define __CLZ 
+
+#define __SADD8                 
+#define __QADD8                 
+#define __SHADD8               
+#define __UADD8                 
+#define __UQADD8              
+#define __UHADD8             
+#define __SSUB8               
+#define __QSUB8                 
+#define __SHSUB8              
+#define __USUB8               
+#define __UQSUB8              
+#define __UHSUB8              
+#define __SADD16              
+#define __QADD16              
+#define __SHADD16             
+#define __UADD16              
+#define __UQADD16             
+#define __UHADD16             
+#define __SSUB16              
+#define __QSUB16              
+#define __SHSUB16             
+#define __USUB16              
+#define __UQSUB16             
+#define __UHSUB16             
+#define __SASX                
+#define __QASX                
+#define __SHASX               
+#define __UASX                
+#define __UQASX               
+#define __UHASX               
+#define __SSAX                
+#define __QSAX                
+#define __SHSAX               
+#define __USAX                
+#define __UQSAX               
+#define __UHSAX               
+#define __USAD8               
+#define __USADA8              
+#define __SSAT16              
+#define __USAT16              
+#define __UXTB16              
+#define __UXTAB16             
+#define __SXTB16              
+#define __SXTAB16             
+#define __SMUAD               
+#define __SMUADX              
+#define __SMLAD               
+#define __SMLADX              
+#define __SMLALD              
+#define __SMLALDX             
+#define __SMUSD               
+#define __SMUSDX              
+#define __SMLSD               
+#define __SMLSDX              
+#define __SMLSLD                
+#define __SMLSLDX              
+#define __SEL                   
+#define __QADD                  
+#define __QSUB                  
+#define __SSAT
+
+#endif
+#ifdef __cplusplus
+}
+#endif
--- CMatrixs_unit_test/main.cpp
+++ CMatrixs_unit_test/main.cpp
@@ -1,9 +1,99 @@
 #include <iostream>
+#include "Matrixs.h"
+#include "Matrix.h"
 using namespace std;
+void inverseTest();
+void add_test();
+
 int main()
 {
+	cout << "start" <<endl;    
+    inverseTest();
+    add_test();
+    cin.get();
 	return 0;
- 	cin.get();
+ 	
+}
+
+void inverseTest()
+{
+    cout << "=============== inverse test ==============" << endl;
+    const uint16_t MAT_SIZE = 4;
+    const float matcs0[MAT_SIZE * MAT_SIZE] = { 0.5f, -0.85f, 0, 20000, 0.85f, 0.5f, 0, 300, 0, 0, 1, 0, 0, 0, 0, 1 };
+    double matcs[MAT_SIZE * MAT_SIZE];
+    float mats0[MAT_SIZE * MAT_SIZE];
+    float mats1[MAT_SIZE * MAT_SIZE];
+    float mats2[MAT_SIZE * MAT_SIZE];
+
+    for (int i = 0; i < MAT_SIZE * MAT_SIZE; i++)
+    {
+        matcs[i] = matcs0[i];
+    }
+    CMatrixs mat0(MAT_SIZE, mats0);
+    mat0.SetData(matcs0);
+    CMatrixs mat1(MAT_SIZE, mats1);
+    CMatrixs mat2(MAT_SIZE, mats2);
+
+    mat0.InvertGaussJordan(mat1);
+    mat0.SetData(matcs0);
+    mat1.mult(mat0, mat2);
+    mat0.print("mat0");
+    mat1.print("mat1");
+    mat2.print("mat2");
+    mat2.printRaw("mat2");
+
+    CMatrix mat3(MAT_SIZE);
+    CMatrix mat4(MAT_SIZE);
+    CMatrix mat5(MAT_SIZE);
+    mat3.SetData((const double*)matcs);
+    mat4.SetData((const double*)matcs);
+    mat3.InvertGaussJordan();
+    mat5 = mat3 * mat4;
+
+    cout << "mat3" << endl;
+    mat3.print();
+    cout << "mat4" << endl;
+    mat4.print();
+    cout << "mat5" << endl;
+    mat5.print();
+}
+
+void add_test()
+{
+    cout << "=============== add test ==============" << endl;
+    const uint16_t MAT_SIZE = 3;
+    float mats0[MAT_SIZE * MAT_SIZE];
+    float mats1[MAT_SIZE * MAT_SIZE];
+    float mats2[MAT_SIZE * MAT_SIZE];
+    float mats3[MAT_SIZE * MAT_SIZE];
+    float mats4[MAT_SIZE * MAT_SIZE];
+    float mats5[MAT_SIZE * MAT_SIZE];
+
+    CMatrixs mat0(MAT_SIZE, mats0);
+    CMatrixs mat1(MAT_SIZE, mats1);
+    CMatrixs mat2(MAT_SIZE, mats2);
+    CMatrixs mat3(MAT_SIZE, mats3);
+    CMatrixs mat4(MAT_SIZE, mats4);
+    CMatrixs mat5(MAT_SIZE, mats5);
+
+    mat0.MakeUnitMatrix();
+    mat1.MakeUnitMatrix();
+    mat2.MakeUnitMatrix();
+    mat3.MakeUnitMatrix();
+
+    mat0.print("mat0");
+    mat1.MakeUnitMatrix();
+    mat1 += mat0;
+
+    mat1.print("mat1");
+
+    cout << "mat3 = mat2 + mat0" << endl;
+    mat2.add(mat0, mat3);
+    mat0.print("mat0");
+    mat2.print("mat2");
+    mat3.print("mat3");
+
+
 }
 //end of file
 
